1ed5b73918de37f9f9893b003d2ac84b98e8963f
augustof <augustobfanck@gmail.com>
2023-10-22 15:55:46 -0300

    feat: card working, inputs ok. missing integration

--- src/components/Payment/Card.jsx
+++ src/components/Payment/Card.jsx
@@ -1,68 +1,10 @@
 import Typography from '@mui/material/Typography';
 import styled from 'styled-components';
 import 'react-credit-cards-2/dist/es/styles-compiled.css'
-
 import React, { useState } from 'react';
 import Cards from 'react-credit-cards-2';
-
 import { FormWrapper } from '../PersonalInformationForm/FormWrapper';
-
-function clearNumber(value = "") {
-    return value.replace(/\D+/g, "");
-}
-
-export function formatCreditCardNumber(value) {
-    if (!value) {
-        return value;
-    }
-
-    const clearValue = clearNumber(value);
-    let nextValue;
-
-    switch (issuer) {
-        case "amex":
-            nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-                4,
-                10
-            )} ${clearValue.slice(10, 15)}`;
-            break;
-        case "dinersclub":
-            nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-                4,
-                10
-            )} ${clearValue.slice(10, 14)}`;
-            break;
-        default:
-            nextValue = `${clearValue.slice(0, 4)} ${clearValue.slice(
-                4,
-                8
-            )} ${clearValue.slice(8, 12)} ${clearValue.slice(12, 19)}`;
-            break;
-    }
-
-    return nextValue.trim();
-}
-
-function formatCVC(value, prevValue, allValues = {}) {
-    const clearValue = clearNumber(value);
-    let maxLength = 4;
-
-    if (allValues.number) {
-        maxLength = issuer === "amex" ? 4 : 3;
-    }
-
-    return clearValue.slice(0, maxLength);
-}
-
-export function formatExpirationDate(value) {
-    const clearValue = clearNumber(value);
-
-    if (clearValue.length >= 3) {
-        return `${clearValue.slice(0, 2)}/${clearValue.slice(2, 4)}`;
-    }
-
-    return clearValue;
-}
+import { formatCreditCardNumber, formatCVC, formatExpirationDate } from './utils';
 
 export default function PaymentForm() {
     const [state, setState] = useState({
@@ -74,32 +16,19 @@ export default function PaymentForm() {
         issuer: ''
     });
 
-    const handleCallback = ({ issuer }, isValid) => {
-        if (isValid) {
-          setState({ issuer });
-        }
-      };
-
     const handleInputChange = (evt) => {
-        console.log(evt)
         let { name, value } = evt.target;
 
         if (name === "number") {
             value = formatCreditCardNumber(value);
         } else if (name === "expiry") {
-            value = formatExpirationDate(value);
+             value = formatExpirationDate(value);
         } else if (name === "cvc") {
             value = formatCVC(value);
         }
 
-        setState({[name]: value});
-    };
-
-    // const handleInputChange = (evt) => {
-    //     const { name, value } = evt.target;
-
-    //     setState((prev) => ({ ...prev, [name]: value }));
-    // }
+        setState((prev) => ({ ...prev, [name]: value }));
+    }
 
     const handleInputFocus = (evt) => {
         setState((prev) => ({ ...prev, focus: evt.target.name }));
@@ -120,14 +49,12 @@ export default function PaymentForm() {
                     cvc={state.cvc}
                     name={state.name}
                     focused={state.focus}
-                    callback={handleCallback}
                 />
                 <FormWrapper>
                     <Form>
                         <input
                             type="tel"
                             name="number"
-                            // pattern="[\d| ]{16,22}"
                             required
                             placeholder="Card Number"
                             className="big"
@@ -150,7 +77,6 @@ export default function PaymentForm() {
                             <input
                                 type="tel"
                                 name="expiry"
-                                //pattern="\d\d/\d\d"
                                 required
                                 placeholder="Valid thru"
                                 className="small"
@@ -161,7 +87,6 @@ export default function PaymentForm() {
                             <input
                                 type="tel"
                                 name="cvc"
-                                //pattern="\d{3,4}"
                                 required
                                 placeholder="CVC"
                                 className="small"
