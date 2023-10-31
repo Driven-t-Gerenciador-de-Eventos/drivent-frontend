5fc5bd9f4c4464ca20ad6331d54239a63d5c345c
Luca-Panza <luca.panza.007@gmail.com>
2023-10-31 10:16:04 -0300

    feat: sign-in github button

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,13 +1,15 @@
-import { useState, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import qs from "query-string";
 
 import AuthLayout from '../../layouts/Auth';
 
 import Input from '../../components/Form/Input';
 import Button from '../../components/Form/Button';
 import Link from '../../components/Link';
-import { Row, Title, Label } from '../../components/Auth';
+import { Row, Title, Label, Icon, Github } from '../../components/Auth';
 
 import EventInfoContext from '../../contexts/EventInfoContext';
 import UserContext from '../../contexts/UserContext';
@@ -24,20 +26,67 @@ export default function SignIn() {
   const { setUserData } = useContext(UserContext);
 
   const navigate = useNavigate();
-  
+
   async function submit(event) {
     event.preventDefault();
 
+    console.log("1")
     try {
       const userData = await signIn(email, password);
       setUserData(userData);
-      console.log(userData)
       toast('Login realizado com sucesso!');
       navigate('/dashboard');
     } catch (err) {
       toast('Não foi possível fazer o login!');
     }
-  } 
+  }
+
+    async function handleGithubSignIn() {
+
+      const VITE_API_GITHUB_AUTH_URL = import.meta.env.VITE_API_GITHUB_AUTH_URL;
+      const VITE_API_GITHUB_CLIENT_ID = import.meta.env.VITE_API_GITHUB_CLIENT_ID;
+      const VITE_API_GITHUB_REDIRECT_URI = import.meta.env.VITE_API_GITHUB_REDIRECT_URI;
+
+      const params = {
+        response_type: 'code',
+        scope: 'user',
+        client_id: VITE_API_GITHUB_CLIENT_ID,
+        redirect_uri: VITE_API_GITHUB_REDIRECT_URI,
+      }
+
+      try {
+        const queryStrings = qs.stringify(params);
+        const authorizationUrl = `${VITE_API_GITHUB_AUTH_URL}?${queryStrings}`;
+        window.location.href = authorizationUrl;
+
+      } catch (err) {
+        console.error('Erro durante a autenticação com o GitHub:', err);
+      }
+    }
+
+    window.onload = async () => {
+      const VITE_API_BACK_END_URL = import.meta.env.VITE_API_BACK_END_URL;
+
+      const url = new URL(window.location.href);
+      const code = url.searchParams.get("code");
+    
+      if (code) {
+        try {
+
+          console.log("code enviado para o back-end", code)
+
+          const response = await axios.post(`${VITE_API_BACK_END_URL}/auth/sign-in/github`, { code });
+          setUserData(response.data);
+          navigate('/dashboard');
+          alert("Você está logado, meu chapa! Dá uma olhada no console!");
+        } catch (err) {
+          alert("Ops, deu algum ruim! Dá uma olhada no console!");
+          console.error('Erro durante a autenticação com o GitHub:', err);
+        }
+      }
+    }
+    
+
 
   return (
     <AuthLayout background={eventInfo.backgroundImageUrl}>
@@ -48,12 +97,30 @@ export default function SignIn() {
       <Row>
         <Label>Entrar</Label>
         <form onSubmit={submit}>
-          <Input label="E-mail" type="text" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
-          <Input label="Senha" type="password" fullWidth value={password} onChange={e => setPassword(e.target.value)} />
-          <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>Entrar</Button>
+          <Input
+            label="E-mail"
+            type="text"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            label="Senha"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" color="primary" fullWidth disabled={loadingSignIn}>
+            Entrar
+          </Button>
         </form>
       </Row>
       <Row>
+      <Icon onClick={handleGithubSignIn}>
+          <Github/>
+          <p>GitHub</p>
+      </Icon>
         <Link to="/enroll">Não possui login? Inscreva-se</Link>
       </Row>
     </AuthLayout>
